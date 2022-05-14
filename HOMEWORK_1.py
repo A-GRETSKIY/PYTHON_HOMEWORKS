e8123b2d3739247ef1dc2d43a9952adba50aa58b
A-GRETSKIY <105476663+A-GRETSKIY@users.noreply.github.com>
2022-05-14 14:59:14 +0300

    Add files via upload

---
+++ HOMEWORK_1.py
@@ -0,0 +1,32 @@
+print('Hello world')
+
+print('Glory to Ukraine')
+print('Just do it!')
+print('Red Bull Gives You Wings')
+
+a = input('A=')
+b = input('B=')
+res = float(a) * float(b)
+print('a * b =', res)
+
+c = float(input('c='))
+d = float(input('d='))
+res = c + d
+print('c + d=', res)
+res = c - d
+print('c - d=', res)
+res = c * d
+print('c * d=', res)
+res = c / d
+print('c / d=', res)
+
+import math
+
+r = float(input('Radius='))
+
+res = r*2
+print('Diametr:', 'r*2=', res)
+res = 2 * math.pi * r
+print('Circumference:', '2 * math.pi * r=', res)
+res = math.pi*((r**2))
+print('Area:', 'math.pi*(r**2)=', res)
