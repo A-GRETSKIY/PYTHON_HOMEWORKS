0baefbbbae23184d9c19fc881e25a41a0d4fd612
A-GRETSKIY <105476663+A-GRETSKIY@users.noreply.github.com>
2022-05-16 21:09:22 +0300

    Add files via upload

---
+++ HOMEWORK_1_3.py
@@ -0,0 +1,23 @@
+a = int(str('123'))
+print(a)
+
+a = float(int(123))
+print(a)
+
+a = int(float(123.45))
+print(a)
+
+d = int(input('Введите номер карты: '))
+print('Последние 4 цифры: ', d % 10000)
+
+x = int(input('Введите 3-х значное число: '))
+print(x // 100 + x // 10 % 10 + x % 10)
+
+print('Введите длинну сторон треугольника')
+a = float(input('a= '))
+b = float(input('b= '))
+c = float(input('c= '))
+p = (a + b + c) / 2
+s = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+print('Площадь треугольника =', s)
+
