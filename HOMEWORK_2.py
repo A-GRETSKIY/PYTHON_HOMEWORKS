dd32b5be02458bf096c7973ff65dac0fda1eb226
A-GRETSKIY <105476663+A-GRETSKIY@users.noreply.github.com>
2022-05-19 11:49:54 +0300

    Add files via upload

---
+++ HOMEWORK_2.py
@@ -0,0 +1,16 @@
+x = int(input('Введите число '))
+print(x < 0 and x or ' ')
+
+x = int(input('Введите число '))
+print(x < 20)
+
+x = int(input('Введите число '))
+print(x == 0)
+
+x = int(input('Введите число: '))
+print((x%2 == 0 and 'Число четное') or x%2!=0 and 'Число нечетное')
+
+a = int(input('Введите число A: '))
+b = int(input('Введите число B: '))
+c = int(input('Введите число C: '))
+print('Максимальное число: ', max(a, b, c))
