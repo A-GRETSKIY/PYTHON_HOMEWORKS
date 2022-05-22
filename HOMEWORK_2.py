c0b811241a4111c3b3d528128e37e36adaddb376
A-GRETSKIY <105476663+A-GRETSKIY@users.noreply.github.com>
2022-05-22 12:32:08 +0300

    Update HOMEWORK_2.py

--- HOMEWORK_2.py
+++ HOMEWORK_2.py
@@ -1,14 +1,25 @@
 x = int(input('Введите число '))
-print(x < 0 and x or ' ')
+print(x < 0 and x or 'Больше или равно нулю')
 
 x = int(input('Введите число '))
-print(x < 20)
+print(x < 20 and x or 'Больше или равно 20')
 
 x = int(input('Введите число '))
 print(x == 0)
 
+# x = int(input('Введите число '))
+# print(not bool(x))
+#
+# x = int(input('Введите число '))
+# print(not x)
+
 x = int(input('Введите число: '))
-print((x%2 and 'Число нечетное') or 'Число четное')
+print((x % 2 and 'Число нечетное') or 'Число четное')
+
+# x = int(input('Введите число: '))
+# print(not x % 2)
+# print(bool(x % 2))
+
 
 a = int(input('Введите число A: '))
 b = int(input('Введите число B: '))
