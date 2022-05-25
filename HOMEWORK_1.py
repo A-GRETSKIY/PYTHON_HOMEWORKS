ac5a3628ff653256feb056efcb3259cfc72bc21d
A-GRETSKIY <105476663+A-GRETSKIY@users.noreply.github.com>
2022-05-25 16:26:07 +0300

    Update HOMEWORK_1.py

--- HOMEWORK_1.py
+++ HOMEWORK_1.py
@@ -1,11 +1,17 @@
 print('Hello world')
 
+###
+
 print('Glory to Ukraine\nJust do it!\nRed Bull Gives You Wings')
+
+###
 
 a = input('A=')
 b = input('B=')
 res = float(a) * float(b)
 print('a * b =', res)
+
+###
 
 c = float(input('c='))
 d = float(input('d='))
@@ -17,6 +23,8 @@ res = c * d
 print('c * d=', res)
 res = c / d
 print('c / d=', res)
+
+###
 
 import math
 
