30f2a91fcce40dea0e9db99d36ca54b36a12a2fb
apgre <a.p.gretskiy@mail.com>
2022-05-25 19:15:19 +0300

    HOMEWORK

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-r = float(input('Radius='))
-res = r*2
-print('Diametr:', 'r*2=', res)
-res = 2 * math.pi * r
-print('Circumference:', '2 * math.pi * r=', res)
-res = math.pi*((r**2))
-print('Area:', 'math.pi*(r**2)=', res)
+dx = 0.5
+i = 25
+
+while i <40:
+    print(i)
+    i = i + dx
