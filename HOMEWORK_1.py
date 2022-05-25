7437e84b5e1860a6053ebb86ccea8c6cef043e8f
A-GRETSKIY <105476663+A-GRETSKIY@users.noreply.github.com>
2022-05-25 16:32:00 +0300

    Update HOMEWORK_1.py

--- HOMEWORK_1.py
+++ HOMEWORK_1.py
@@ -1,3 +1,14 @@
+# Homework 1
+# Exercise 1. Write a Python-script that displays the message “Hello world”.
+# Exercise 2. Rewrite the first script to display three any messages.
+# Exercise 3. Write a Python-script to reads values for the length and width of a
+# rectangle and returns the area of the rectangle.
+# Exercise 4. Write a program that requests the user to enter two numbers and
+# prints the sum, product, difference and quotient of the two numbers.
+# Exercise 5. Write a program that reads in the radius of a circle and prints the
+# circle’s diameter, circumference and area. Use the constant value 3.14159 for π.
+# Do these calculations in output statements
+
 print('Hello world')
 
 ###
