7a2e82f25e6f3864aa3455a0cd8f3ef5b268402d
apgre <a.p.gretskiy@mail.com>
2022-05-31 08:30:59 +0300

    CHANGE

--- TEST.py
+++ TEST.py
@@ -1,9 +1,6 @@
-x = input()
-print(x)
-
-x = 4
-y = 4
-print(x + y)
-
-a = [int(i) for i in input()]
-print('Счастливый' if a[0] + a[1] == a[-1] + a[-2] else 'Обычный')
+a = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+print(a[0], a[1], a[2], a[3], sep='\n')
+s = []
+for i in a:
+    s.extend(i)
+print(sum(s))
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-a = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
-print(a[0], a[1], a[2], a[3], sep='\n')
-print(sum(a[0] + a[1] + a[2] + a[3]))
-
+cash = int(input())
+sum = 0
+for i in [100, 20, 10, 5, 1]:
+    sum += cash // i
+    cash = cash % i
+print(sum)
