5a909bb8a9e7301db59602c717afea0fc56264e0
A-GRETSKIY <105476663+A-GRETSKIY@users.noreply.github.com>
2022-06-08 09:14:39 +0300

    Update HOMEWORK_1_1.py

--- HOMEWORK_1_1.py
+++ HOMEWORK_1_1.py
@@ -1,23 +1,22 @@
-# Homework 1
-# Exercise 1. Write a Python-script that displays the message “Hello world”.
-# Exercise 2. Rewrite the first script to display three any messages.
-# Exercise 3. Write a Python-script to reads values for the length and width of a rectangle and returns the area of the rectangle.
-# Exercise 4. Write a program that requests the user to enter two numbers and prints the sum, product, difference and quotient of the two numbers.
-# Exercise 5. Write a program that reads in the radius of a circle and prints the circle’s diameter, circumference and area. Use the constant value 3.14159 for π. Do these calculations in output statements.
+import math
+
+            # 1. Write a Python-script that displays the message “Hello world”.
 
 print('Hello world')
 
-###
+            # 2. Rewrite the first script to display three any messages.
 
 print('Glory to Ukraine\nJust do it!\nRed Bull Gives You Wings')
 
-###
+            # 3. Write a Python-script to reads values for the length and width of a
+            # rectangle and returns the area of the rectangle.
 
 a = input('A=')
 b = input('B=')
 print('a * b =', float(a) * float(b))
 
-###
+            # 4. Write a program that requests the user to enter two numbers and
+            # prints the sum, product, difference and quotient of the two numbers.
 
 c = float(input('c='))
 d = float(input('d='))
@@ -26,9 +25,10 @@ print('c - d=', c - d)
 print('c * d=', c * d)
 print('c / d=', c / d)
 
-###
+            # 5. Write a program that reads in the radius of a circle and prints the
+            # circle’s diameter, circumference and area. Use the constant value 3.14159 for π.
+            # Do these calculations in output statements.
 
-import math
 r = float(input('Radius='))
 print('Diametr:', 'r*2=', r * 2)
 print('Circumference:', '2 * math.pi * r=', 2 * math.pi * r)
