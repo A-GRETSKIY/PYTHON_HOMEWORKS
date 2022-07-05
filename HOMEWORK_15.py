c4eaf3f1e6986f9cfb4997107ffa8d9afe6ffb43
A-GRETSKIY <105476663+A-GRETSKIY@users.noreply.github.com>
2022-07-05 17:22:01 +0300

    Add files via upload

---
+++ HOMEWORK_15.py
@@ -0,0 +1,91 @@
+class Rectangle:
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+
+    def square(self):
+        return self.width * self.height
+
+    def __gt__(self, other):
+        return self.square() > other.square()
+
+    def __lt__(self, other):
+        return self.square() < other.square()
+
+    def __ge__(self, other):
+        return self.square() >= other.square()
+
+    def __le__(self, other):
+        return self.square() <= other.square()
+
+    def __add__(self, other):
+        return self.square() + other.square()
+
+    def __mul__(self, n):
+        return self.square() * n
+
+    def __str__(self):
+        return f'{self.width} x {self.height}'
+
+
+rec_1 = Rectangle(2, 3)
+rec_2 = Rectangle(5, 6)
+
+print(rec_1 > rec_2)
+print(rec_1 + rec_2)
+print(rec_1 * 5)
+
+
+
+class Fraction(object):
+
+    def __init__(self, numerator, denominator):
+        if denominator == 0:
+            raise ZeroDivisionError("denominator cannot be zero")
+        elif type(numerator) is not int or type(denominator) is not int:
+            raise TypeError("numerator and denominator must be integers")
+
+        def gcd(a, b):
+            while b != 0:
+                a, b = b, a % b
+            return a
+
+        common_factor = gcd(numerator, denominator)
+        self.numer = numerator // common_factor
+        self.denom = denominator // common_factor
+
+    def __str__(self):
+        if self.denom != 1:
+            return f"{self.numer}/{self.denom}"
+        else:
+            return str(self.numer)
+
+    def __float__(self):
+        return self.numer / self.denom
+
+    def __eq__(self, other):
+        if type(other) is Fraction:
+            return self.numer == other.numer and self.denom == other.denom
+        else:
+            return float(self) == other
+
+    def __add__(self, other):
+        return Fraction(self.numer * other.numer + self.denom * other.denom,
+                        self.denom * other.denom)
+
+    def __sub__(self, other):
+        return Fraction(self.numer * other.numer - self.denom * other.denom,
+                        self.denom * other.denom)
+
+    def __mul__(self, other):
+        return Fraction(self.numer * other.numer, self.denom * other.denom)
+
+    def __truediv__(self, other):
+        if self.denom * other.numer == 0:
+            raise ZeroDivisionError("cannot divide by zero")
+        return Fraction(self.numer * other.denom, self.denom * other.numer)
+
+
+num_1 = Fraction(2, 3)
+num_2 = Fraction(1, 2)
+print(num_1 - num_2)
